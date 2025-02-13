3ebf984c63fae7a176ea1013deb0e837954307ab
Kapka Kaloyanova <146618996+KapkaKaloyanova@users.noreply.github.com>
2025-02-13 21:11:21 +0200

    Add create movie
    Add uuid

--- src/services/movie-service.js
+++ src/services/movie-service.js
@@ -1,13 +1,24 @@
+import { v4 as uuid } from 'uuid';
+
 import movies from '../movies.js';
 
-const movieService = {
+export default {
     findOne(movieId) {
         //TODO: if movie is missing?
 
         const result = movies.find(movie => movie.id === movieId);
 
         return result;
+    },
+
+    create(movieData) {
+        const newId = uuid();
+
+        movies.push({
+            id: newId,
+            ...movieData
+        });
+        return newId;
     }
 }
 
-export default movieService;
